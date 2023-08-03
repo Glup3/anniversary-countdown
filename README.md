1bac6807578cf5081c6fa1e219813b8f6be9336c
Phuc Tran <glup3.tran@gmail.com>
2023-08-03 15:38:19 +0200

    PWA enhancement

--- README.md
+++ README.md
@@ -4,7 +4,15 @@ I love my Hime-sama
 
 ## How to get started
 
-1. `fnm use`
-2. `pnpm install`
-3. `pnpm dev`
-4. `http://localhost:5173`
+```
+1. fnm use
+2. pnpm install
+3. pnpm dev
+4. http://localhost:5173
+```
+
+## PWA generate images
+
+```
+npx pwa-assets-generator --preset minimal public/annaversarylogo.png
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+
+const manifestForPWA: Partial<VitePWAOptions> = {
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    manifest: {
+        name: 'Annaversary',
+        short_name: 'Annaversary',
+        description: 'All Annaversaries in one place',
+        start_url: '/',
+        theme_color: '#f9f9f9',
+        icons: [
+            {
+                src: 'pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+            },
+            {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any',
+            },
+            {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable',
+            },
+        ],
+    },
+    devOptions: {
+        enabled: true,
+    },
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), VitePWA(manifestForPWA)],
     base: '/anniversary-countdown/',
 })
